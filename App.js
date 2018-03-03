660ab2679de47a5b0203a0c5c68d6478d83f8fed
Blake Lovelace <lovelace.b@hotmail.com>
2018-03-03 11:23:33 +0900

    Install ble le plugin and scanning and connecting working

--- App.js
+++ App.js
@@ -12,8 +12,11 @@ import {
   View,
   TextInput,
   Button,
-  FlatList
+  FlatList,
+  TouchableHighlight
 } from 'react-native';
+
+import { BleManager } from 'react-native-ble-plx';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,14 +25,19 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+// const bleManager = new BleManager();
+
 export default class App extends Component<{}> {
   constructor() {
     super();
+    this.manager = new BleManager();
     this.state = {
       helloText: "Hello",
       showTextField: false,
       list: [{key: 'Initial', value: "Initial"}, {key: 'Values', value: "Values"}, {key: 'To', value: "To"}, {key: 'The', value: 'The'}, {key: 'Flatlist', value: 'Flatlist'}],
-      count: 0
+      count: 0,
+      buttonTitle: 'Start Scan',
+      connectedDevice: null
     }
   }
 
@@ -43,6 +51,55 @@ export default class App extends Component<{}> {
     })
   }
 
+  connectToDevice(device) {
+    if (!this.state.connectedDevice) {
+      this.manager.connectToDevice(device.id).then((res) => {
+        // console.log('connect res', res);
+        this.setState({connectedDevice: res});
+      });
+    } else {
+      this.state.connectedDevice.cancelConnection().then((res) => {
+        console.log('device.cancelConnection res', res);
+        this.setState({connectedDevice: null});
+      });
+    }
+  }
+
+  startScan(event) {
+    // this.manager.state().then((res) => {
+    //   console.log("hello worldss " + res);
+    // });
+    if (!this.state.scanning) {
+      this.manager.startDeviceScan(null, null, (error, device) => {
+        // console.log('device', device);
+        // console.log('device name', device.name);
+        // console.log('device id', device.id);
+        if (device.name) {
+          console.log('device name', device.name);
+          var deviceList = this.state.list;
+
+          if (deviceList.find((element) => {
+            return element.id === device.id
+          })) {
+            return;
+          }
+
+          var newDevice = {
+            id: device.id,
+            name: device.name
+          }
+
+          this.setState({list: deviceList.concat(newDevice)});
+        }
+      })
+      this.setState({scanning: true, buttonTitle: 'Stop Scan'});
+    } else {
+      this.manager.stopDeviceScan()
+      this.setState({scanning: false, buttonTitle: 'Start Scan'});
+    }
+
+  }
+
   addToList(event) {
     this.setState({
       list: this.state.list.concat([{key: this.state.helloText + this.state.count, value: this.state.helloText}]),
@@ -51,12 +108,33 @@ export default class App extends Component<{}> {
   }
 
   render() {
+    // { connectedDevice } = this.state;
+
+    var isConnected = null;
+    if (this.state.connectedDevice) {
+      isConnected = (
+        <Text>Connected To: {this.state.connectedDevice.name}</Text>
+      );
+    }
+
     return (
        <View style={styles.container}>
         <View style={styles.listContainer}>
           <FlatList
             data={this.state.list}
-            renderItem={({item}) => <Text>{item.value}</Text>}
+            // renderItem={({item}) => <Text>{item.value}</Text>}
+            keyExtractor={(item) => item.id}
+            renderItem={
+              ({item}) => {
+                return (
+                  <TouchableHighlight
+                  underlayColor="green"
+                  onPress={() => {this.connectToDevice(item)}}>
+                    <Text style={{margin: 30}}>{item.name}</Text>
+                  </TouchableHighlight>
+                )
+              }
+            }
           />
         </View>
         {/*
@@ -64,9 +142,9 @@ export default class App extends Component<{}> {
           <Button style={{borderWidth: 1}} onPress={this.helloWorld.bind(this)} title={this.state.helloText} color="#841584"></Button>
         */}
         <View style={styles.inputContainer}>
-          <TextInput style={{width: 300}} onChangeText={(newText) => {this.setState({helloText: newText})}} value={this.state.helloText}/>
-          <Button style={{padding: 0, height: 50}} onPress={this.addToList.bind(this)} title="Add!"></Button>
-          <Button style={{padding: 0, height: 50}} onPress={this.startScan.bind(this)} title="StartScan"></Button>
+          {/*<TextInput style={{width: 300}} onChangeText={(newText) => {this.setState({helloText: newText})}} value={this.state.helloText}/>*/}
+          {/*<Button style={{padding: 0, height: 50}} onPress={this.addToList.bind(this)} title="Add!"></Button>*/}
+          <Button style={{padding: 0, height: 50}} onPress={this.startScan.bind(this)} title={this.state.buttonTitle}></Button>
         </View>
       </View>
     );
